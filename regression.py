b8f200d8aed872419222c16b05ce6bf850b654c2
Gowtham Behara <gothsiddu@gmail.com>
2018-04-19 07:50:59 +0530

    Add forecast_col and forecast_out

--- regression.py
+++ regression.py
@@ -1,5 +1,8 @@
 import pandas as pd
-import quandl
+import numpy as np
+from sklearn import preprocessing, cross_decomposition, svm
+from sklearn.linear_model import LinearRegression
+import quandl, math
 
 df = quandl.get("WIKI/GOOGL")
 print(df.head())
@@ -10,3 +13,12 @@ df['PCT_Change'] = (df['Adj. Close'] - df['Adj. Open']) / (df['Adj. Open'] * 100
 
 df = df[['Adj. Close', 'HL_PCT', 'PCT_Change', 'Adj. Volume']]
 print(df.head())
+
+forecast_col = 'Adj. Close'
+# -99999 acts as outlier; here we train data points with missing values too, as an outlier.
+df.fillna(value=-99999, inplace=True)
+
+# forecast range 1% of the total length of duration in data set.
+forecast_out = int(math.ceil(0.01 * len(df)))
+
+df['label'] = df[forecast_col].shift(-forecast_out)
