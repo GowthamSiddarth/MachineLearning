d7f96ba074cc5e4489ad85941e4bc65f9d45dc0c
Gowtham Behara <gothsiddu@gmail.com>
2018-04-19 08:26:19 +0530

    Add preprocessing and classify with multiple classifiers

--- regression.py
+++ regression.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
-from sklearn import preprocessing, cross_decomposition, svm
+from sklearn import preprocessing, svm
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import quandl, math
 
 df = quandl.get("WIKI/GOOGL")
-print(df.head())
+# print(df.head())
 
 df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
 df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / (df['Adj. Low'] * 100)
@@ -22,3 +23,32 @@ df.fillna(value=-99999, inplace=True)
 forecast_out = int(math.ceil(0.01 * len(df)))
 
 df['label'] = df[forecast_col].shift(-forecast_out)
+
+df.dropna(inplace=True)
+
+# drop label values along the column: axis = 1
+X = np.array(df.drop(['label'], axis=1))
+
+# scale values between -1 and 1
+X = preprocessing.scale(X)
+
+y = np.array(df['label'])
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+clf = svm.SVR()
+clf.fit(X=X_train, y=y_train)
+confidence = clf.score(X=X_test, y=y_test)
+print(confidence)
+
+# n_jobs is the number of threads to work on for the classifier
+clf = LinearRegression(n_jobs=-1)
+clf.fit(X=X_train, y=y_train)
+confidence = clf.score(X=X_test, y=y_test)
+print(confidence)
+
+for k in ['linear', 'poly', 'rbf', 'sigmoid']:
+    clf = svm.SVR(kernel=k)
+    clf.fit(X=X_train, y=y_train)
+    confidence = clf.score(X=X_test, y=y_test)
+    print(k, confidence)
