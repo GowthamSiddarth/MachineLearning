c64452766a02c533a3cf838073c108ddd50e4de7
Gowtham Behara <gothsiddu@gmail.com>
2018-04-24 10:00:17 +0530

    Correct spelling of k_nearest_neighbors

--- knearestneighborsfromscratch.py
+++ knearestneighborsfromscratch.py
@@ -4,7 +4,7 @@ import warnings
 from collections import Counter
 
 
-def k_nearest_neighbots(data_set, predict, k=3):
+def k_nearest_neighbors(data_set, predict, k=3):
     if len(data_set) >= k:
         warnings.warn("")
 
@@ -26,7 +26,7 @@ new_features = [5, 7]
 plt.scatter(new_features[0], new_features[1], s=100)
 plt.show()
 
-result = k_nearest_neighbots(data_set, new_features)
+result = k_nearest_neighbors(data_set, new_features)
 print(result)
 
 [[plt.scatter(features[0], features[1], s=100, c=category) for features in data_set[category]] for category in
