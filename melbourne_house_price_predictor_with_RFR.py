9818a98e74253a8005cd33b05af7c9107d00e3e1
Gowtham Behara <gothsiddu@gmail.com>
2018-05-14 00:35:01 +0530

    Write predictions into a csv file for each test case

--- melbourne_house_price_predictor_with_RFR.py
+++ melbourne_house_price_predictor_with_RFR.py
@@ -19,9 +19,9 @@ train_X, dev_X, train_y, dev_y = train_test_split(X, y, random_state=0)
 model = RandomForestRegressor()
 model.fit(train_X, train_y)
 
-predictions = model.predict(dev_X)
-mae = mean_absolute_error(dev_y, predictions)
-print("MEAN ABSOLUTE ERROR: " + str(mae))
+dev_predictions = model.predict(dev_X)
+dev_mae = mean_absolute_error(dev_y, dev_predictions)
+print("MEAN ABSOLUTE ERROR: " + str(dev_mae))
 
 test_data_file = 'data/melbourne-house-prices-test-data.csv'
 test_data = pd.read_csv(test_data_file)
@@ -29,3 +29,6 @@ test_data = pd.read_csv(test_data_file)
 test_X = test_data[features]
 predictions = model.predict(test_X)
 print(predictions)
+
+my_predictions = pd.DataFrame({'Id': test_data.Id, 'SalePrice': predictions})
+my_predictions.to_csv('data/melbourne-house-prices-predictions.csv', index=False)
