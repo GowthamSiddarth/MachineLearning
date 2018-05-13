b3b0c48dc249e5564068b38386b9b3b81bb721c0
Gowtham Behara <gothsiddu@gmail.com>
2018-05-13 23:29:38 +0530

    Use column headers/labels to filter columns from data

--- melbourne_house_price_predictor.py
+++ melbourne_house_price_predictor.py
@@ -4,3 +4,11 @@ train_data_file = 'data/melbourne-house-prices-train-data.csv'
 data = pd.read_csv(train_data_file)
 
 print(data.describe())
+
+columns = data.columns
+print(columns)
+
+interested_columns_labels = ['YearBuilt', '1stFlrSF']
+interested_columns_data = data[interested_columns_labels]
+
+print(interested_columns_data.describe())
