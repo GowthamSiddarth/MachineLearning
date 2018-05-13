194f8c061874ad912499c46bf1df474fe3112a41
Gowtham Behara <gothsiddu@gmail.com>
2018-05-14 00:30:35 +0530

    Use testdata.csv to make predictions and split input data into train and dev_set

--- melbourne_house_price_predictor_with_RFR.py
+++ melbourne_house_price_predictor_with_RFR.py
@@ -4,21 +4,28 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 
 train_data_file = 'data/melbourne-house-prices-train-data.csv'
-data = pd.read_csv(train_data_file)
-print(data.describe())
+train_data = pd.read_csv(train_data_file)
+print(train_data.describe())
 
-columns = data.columns
+columns = train_data.columns
 print(columns)
 
 features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
-X, y = data[features], data.SalePrice
+X, y = train_data[features], train_data.SalePrice
 print(X.describe())
 
-train_X, test_X, train_y, test_y = train_test_split(X, y, random_state=0)
+train_X, dev_X, train_y, dev_y = train_test_split(X, y, random_state=0)
 
 model = RandomForestRegressor()
 model.fit(train_X, train_y)
 
-predictions = model.predict(test_X)
-mae = mean_absolute_error(test_y, predictions)
+predictions = model.predict(dev_X)
+mae = mean_absolute_error(dev_y, predictions)
 print("MEAN ABSOLUTE ERROR: " + str(mae))
+
+test_data_file = 'data/melbourne-house-prices-test-data.csv'
+test_data = pd.read_csv(test_data_file)
+
+test_X = test_data[features]
+predictions = model.predict(test_X)
+print(predictions)
