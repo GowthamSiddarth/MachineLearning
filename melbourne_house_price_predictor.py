cd7d54a40cff19238acde275a4a76ebb7726d4e9
Gowtham Behara <gothsiddu@gmail.com>
2018-05-14 00:16:21 +0530

    Calculate MAE with max_leaf_nodes with diff values of max_leaf_nodes to find the optimal max_leaf_nodes

--- melbourne_house_price_predictor.py
+++ melbourne_house_price_predictor.py
@@ -3,6 +3,14 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 
+
+def get_mae(max_leaf_nodes, train_X, test_X, train_y, test_y):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_X, train_y)
+    predictions = model.predict(test_X)
+    return mean_absolute_error(test_y, predictions)
+
+
 train_data_file = 'data/melbourne-house-prices-train-data.csv'
 data = pd.read_csv(train_data_file)
 print(data.describe())
@@ -25,3 +33,7 @@ print(predictions)
 
 mae = mean_absolute_error(predictions, test_y)
 print("MEAN ABSOLUTE ERROR: " + str(mae))
+
+for max_leaf_nodes in [5, 50, 500, 5000]:
+    mae = get_mae(max_leaf_nodes, train_X, test_X, train_y, test_y)
+    print("Max leaf nodes: %d   \t\tMean Absolute Error: %d" % (max_leaf_nodes, mae))
