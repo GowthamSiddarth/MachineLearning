191a9520d054b721822c4eff483039e64cbef596
Gowtham Behara <gothsiddu@gmail.com>
2018-05-14 00:05:47 +0530

    Split data into train and test to calculate MAE on test data

--- melbourne_house_price_predictor.py
+++ melbourne_house_price_predictor.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
 
 train_data_file = 'data/melbourne-house-prices-train-data.csv'
 data = pd.read_csv(train_data_file)
@@ -13,12 +14,14 @@ features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'Bedroom
 X, y = data[features], data.SalePrice
 print(X.describe())
 
+train_X, test_X, train_y, test_y = train_test_split(X, y, random_state=0)
+
 model = DecisionTreeRegressor()
-model.fit(X, y)
+model.fit(train_X, train_y)
 
 print("Predictions:")
-predictions = model.predict(X)
+predictions = model.predict(test_X)
 print(predictions)
 
-mae = mean_absolute_error(predictions, y)
+mae = mean_absolute_error(predictions, test_y)
 print("MEAN ABSOLUTE ERROR: " + str(mae))
