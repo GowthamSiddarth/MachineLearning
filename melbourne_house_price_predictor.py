ab7f6e3fd8ea4a3fdc9624fe94f8bfc8429ed183
Gowtham Behara <gothsiddu@gmail.com>
2018-05-13 23:57:54 +0530

    Initialize DecisionTreeRegressor and predict for sample train data

--- melbourne_house_price_predictor.py
+++ melbourne_house_price_predictor.py
@@ -1,16 +1,22 @@
 import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
 
 train_data_file = 'data/melbourne-house-prices-train-data.csv'
 data = pd.read_csv(train_data_file)
-
 print(data.describe())
 
 columns = data.columns
 print(columns)
 
 features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
-X = data[features]
-
+X, y = data[features], data.SalePrice
 print(X.describe())
 
-y = data.SalePrice
+model = DecisionTreeRegressor()
+model.fit(X, y)
+
+print("Make predictions for following 5 houses")
+print(X.head())
+
+print("Predictions:")
+print(model.predict(X.head()))
