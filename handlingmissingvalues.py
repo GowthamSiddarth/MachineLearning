24fe09886a398bc35d34e5f7d2d446dd37678983
Gowtham Behara <gothsiddu@gmail.com>
2018-05-14 23:23:41 +0530

    Basic prblm set up to compare methods of handling missing values

--- handlingmissingvalues.py
+++ handlingmissingvalues.py
@@ -1,29 +1,12 @@
 import pandas as pd
 
+melb_data = pd.read_csv('data/melb_data.csv')
 
-def get_train_and_test_data():
-    train_data = pd.read_csv('data/melbourne-house-prices-train-data.csv')
-    test_data = pd.read_csv('data/melbourne-house-prices-test-data.csv')
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
 
-    return train_data, test_data
+melb_target = melb_data.Price
+melb_predictors = melb_data.drop(['Price'], axis=1)
 
-
-def drop_columns_with_missing_values(train_data, test_data):
-    cols_with_missing_values = [col for col in train_data.columns if train_data[col].isnull().any()]
-
-    reduced_train_data = train_data.drop(cols_with_missing_values, axis=1)
-    reduced_test_data = test_data.drop(cols_with_missing_values, axis=1)
-
-    return reduced_train_data, reduced_test_data
-
-
-if __name__ == '__main__':
-    train_data, test_data = get_train_and_test_data()
-
-    print(train_data.describe())
-    print(test_data.describe())
-
-    reduced_train_data, reduced_test_data = drop_columns_with_missing_values(train_data, test_data)
-
-    print(reduced_train_data.describe())
-    print(reduced_test_data.describe())
+melb_numeric_predictors = melb_predictors.select_dtypes(exclude=['object'])
