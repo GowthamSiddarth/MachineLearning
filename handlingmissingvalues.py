224d7fa1680c636d706f64008e785e3c97a817a7
Gowtham Behara <gothsiddu@gmail.com>
2018-05-14 22:24:03 +0530

    Drop same columns from both training and test data set

--- handlingmissingvalues.py
+++ handlingmissingvalues.py
@@ -8,8 +8,22 @@ def get_train_and_test_data():
     return train_data, test_data
 
 
+def drop_columns_with_missing_values(train_data, test_data):
+    cols_with_missing_values = [col for col in train_data.columns if train_data[col].isnull().any()]
+
+    reduced_train_data = train_data.drop(cols_with_missing_values, axis=1)
+    reduced_test_data = test_data.drop(cols_with_missing_values, axis=1)
+
+    return reduced_train_data, reduced_test_data
+
+
 if __name__ == '__main__':
     train_data, test_data = get_train_and_test_data()
 
     print(train_data.describe())
     print(test_data.describe())
+
+    reduced_train_data, reduced_test_data = drop_columns_with_missing_values(train_data, test_data)
+
+    print(reduced_train_data.describe())
+    print(reduced_test_data.describe())
