393201f2dce74e9a38586e1037ca8fb3a52dbc62
Gowtham Behara <gothsiddu@gmail.com>
2018-05-14 23:32:38 +0530

    Use score_dataset to get mae of data handled with missing method

--- handlingmissingvalues.py
+++ handlingmissingvalues.py
@@ -6,7 +6,18 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 
+
+def score_dataset(X_train, y_train, X_test, y_test):
+    model = RandomForestRegressor()
+    model.fit(X_train, y_train)
+    predictions = model.predict(X_test)
+    return mean_absolute_error(y_test, predictions)
+
+
 melb_target = melb_data.Price
 melb_predictors = melb_data.drop(['Price'], axis=1)
 
 melb_numeric_predictors = melb_predictors.select_dtypes(exclude=['object'])
+
+train_X, test_X, train_y, test_y = train_test_split(melb_numeric_predictors, melb_target,
+                                                    train_size=0.7, test_size=0.3, random_state=0)
