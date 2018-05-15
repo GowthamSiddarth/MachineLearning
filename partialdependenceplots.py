1679aaccb4e3a82dc2a4414f5d6524a9f993c971
Gowtham Behara <gothsiddu@gmail.com>
2018-05-15 22:43:56 +0530

    Plot partial dependence graphs for titanic data

--- partialdependenceplots.py
+++ partialdependenceplots.py
@@ -3,14 +3,15 @@ from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.ensemble.partial_dependence import plot_partial_dependence
 from matplotlib import pyplot as plt
 
-partial_dependence_features_labels = ['Distance', 'Landsize', 'BuildingArea']
+partial_dependence_features_labels = ['Age', 'Fare']
 
 
 def get_data():
-    data_file_path = 'data/melb_data.csv'
+    data_file_path = 'data/titanic-train.csv'
     data = pd.read_csv(data_file_path)
-    y = data.Price
+    y = data.Survived
     X = data[partial_dependence_features_labels]
+    X = pd.get_dummies(X)
 
     from sklearn.preprocessing import Imputer
     imputer = Imputer()
@@ -22,6 +23,7 @@ def get_data():
 X, y = get_data()
 model = GradientBoostingRegressor()
 model.fit(X, y)
-plots = plot_partial_dependence(model, feature_names=partial_dependence_features_labels, features=[0, 2], X=X,
-                                grid_resolution=10)
+fig, axes = plot_partial_dependence(model, feature_names=partial_dependence_features_labels, features=[0, 1], X=X,
+                                    grid_resolution=10)
+
 plt.show()
