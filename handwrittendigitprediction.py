748a62267932c7217758449c97251012924dc18f
Gowtham Behara <gothsiddu@gmail.com>
2018-05-16 09:00:06 +0530

    Preprocess images from grayscale to binary

--- handwrittendigitprediction.py
+++ handwrittendigitprediction.py
@@ -4,16 +4,15 @@ from sklearn.svm import SVC
 from matplotlib import pyplot as plt
 
 
-def view_image(data_set, loc):
-    img = data_set.iloc[loc, 1:].as_matrix()
-    img = img.reshape((28, 28))
-
+def view_image(img, label):
     plt.figure(1)
-    plt.imshow(img, cmap='gray')
-    plt.title(data_set.iloc[loc, 0])
+    plt.hist(img)
 
     plt.figure(2)
-    plt.hist(data_set.iloc[loc, 1:])
+    img = img.as_matrix()
+    img = img.reshape((28, 28))
+    plt.imshow(img, cmap='binary')
+    plt.title(label)
 
     plt.show()
 
@@ -24,7 +23,10 @@ test_data = pd.read_csv('data/hand-writtern-digit-recognition-test.csv')
 X, y = train_data.iloc[:5000, 1:], train_data.iloc[:5000, :1]
 train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.2, random_state=0)
 
-# view_image(train_data, 9)
+train_X[train_X > 0] = 1
+test_X[test_X > 0] = 1
+
+view_image(train_X.iloc[9], train_y.iloc[9])
 clf = SVC()
 clf.fit(train_X, train_y.values.ravel())
 score = clf.score(test_X, test_y)
