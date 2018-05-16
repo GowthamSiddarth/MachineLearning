1b17997075ccaddc66806a177048060172fd8c98
Gowtham Behara <gothsiddu@gmail.com>
2018-05-16 09:08:29 +0530

    Run predictions on test data & store in a csv

--- handwrittendigitprediction.py
+++ handwrittendigitprediction.py
@@ -26,9 +26,20 @@ train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.2, random_
 train_X[train_X > 0] = 1
 test_X[test_X > 0] = 1
 
-view_image(train_X.iloc[9], train_y.iloc[9])
+# view_image(train_X.iloc[9], train_y.iloc[9])
+
 clf = SVC()
 clf.fit(train_X, train_y.values.ravel())
 score = clf.score(test_X, test_y)
 
 print(score)
+
+test_data[test_data > 0] = 1
+results = clf.predict(test_data[:5000])
+
+results_df = pd.DataFrame(results)
+results_df.index += 1
+results_df.index.name = 'ImageID'
+results_df.columns = ['Label']
+
+results_df.to_csv('data/hand-written-digit-recognition-results.csv', header=True)
