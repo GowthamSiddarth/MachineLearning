244af5ba13df058ce79004df5d3f70d306f66edb
Gowtham Behara <gothsiddu@gmail.com>
2018-05-16 23:56:47 +0530

    Load train & test data

--- handwrittendigitspredictionwithKeras.py
+++ handwrittendigitspredictionwithKeras.py
@@ -8,6 +8,10 @@ from keras.layers import Dense, Dropout, Lambda, Flatten
 from keras.optimizers import Adam, RMSprop
 from sklearn.model_selection import train_test_split
 
-from subprocess import check_output
+train_data = pd.read_csv('data/hand-written-digit-recognition-train.csv')
+test_data = pd.read_csv('data/hand-writtern-digit-recognition-test.csv')
 
-print(check_output(["ls", "data/"]).decode("utf8"))
+X_train = train_data.ix[:, 1:].values.as_type('float32')
+y_train = train_data.ix[:, :1].values.as_type('int32')
+
+X_test = test_data.values.as_type('float32')
