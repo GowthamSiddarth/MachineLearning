89b6cef615b56801196d5cddcba93dc13b53b669
Gowtham Behara <gothsiddu@gmail.com>
2018-05-20 17:44:11 +0530

    Calculate RMSE & RSE using sklearn

--- scratch/linearregression/ordinary_least_square_from_sklearn.py
+++ scratch/linearregression/ordinary_least_square_from_sklearn.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import numpy as np
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 from matplotlib import pyplot as plt
 
 plt.rcParams['figure.figsize'] = (20.0, 10.0)
@@ -10,8 +12,16 @@ print(data.head())
 X = data.ix[:, 2]
 Y = data.ix[:, 3]
 
+X = X.values.reshape(X.shape[0], 1)
+Y = Y.values.reshape(Y.shape[0], 1)
+
 model = LinearRegression()
 model.fit(X, Y)
 
 y = model.predict(X)
-print(y)
+
+rmse = np.sqrt(mean_squared_error(Y, y))
+print("root mean squared error = " + str(rmse))
+
+rse = model.score(X, Y)
+print("root squared error = " + str(rse))
