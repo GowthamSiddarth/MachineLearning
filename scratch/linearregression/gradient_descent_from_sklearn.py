85c32523738dfcaeff07e69e132ccb7576a54ec6
Gowtham Behara <gothsiddu@gmail.com>
2018-05-20 22:35:33 +0530

    Preprocess data before training in mode;

--- scratch/linearregression/gradient_descent_from_sklearn.py
+++ scratch/linearregression/gradient_descent_from_sklearn.py
@@ -1,5 +1,17 @@
 import pandas as pd
+import numpy as np
 
 data = pd.read_csv('../../data/student.csv')
-print(data.shape)
 print(data.head())
+
+m = data.shape[0]
+math = data['Math'].values.reshape(m, 1)
+read = data['Reading'].values.reshape(m, 1)
+write = data['Writing'].values.reshape(m, 1)
+
+math = (math - np.mean(math)) / np.std(math)
+read = (read - np.mean(read)) / np.std(read)
+write = (write - np.mean(write)) / np.std(write)
+
+X = np.c_[math, read]
+Y = write
