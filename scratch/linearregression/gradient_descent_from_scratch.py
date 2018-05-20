c0831473567d6b950b450c51369080a5eccda343
Gowtham Behara <gothsiddu@gmail.com>
2018-05-20 20:24:18 +0530

    Preprocess input data and implement gradient descent

--- scratch/linearregression/gradient_descent_from_scratch.py
+++ scratch/linearregression/gradient_descent_from_scratch.py
@@ -8,6 +8,21 @@ def cost_function(x, y, beta):
     return (np.dot((np.dot(x, beta) - y).T, np.dot(x, beta) - y) / y.shape[0]).ravel()
 
 
+def gradient_descent(x, y, beta, alpha, iterations):
+    m = y.shape[0]
+
+    for iteration in range(1, iterations + 1):
+        h = np.dot(x, beta)
+        loss = h - y
+        gradient = np.dot(x.T, loss) / m
+        beta = beta - alpha * gradient
+
+        cost = cost_function(x, y, beta)
+        print("Cost at iteration " + str(iteration) + " : " + str(cost))
+
+    return beta, cost
+
+
 data = pd.read_csv('../../data/student.csv')
 print(data.head())
 
@@ -16,6 +31,10 @@ math = data['Math'].values.reshape((m, 1))
 read = data['Reading'].values.reshape((m, 1))
 write = data['Writing'].values.reshape((m, 1))
 
+math = (math - np.mean(math)) / np.std(math)
+read = (read - np.mean(read)) / np.std(read)
+write = (write - np.mean(write)) / np.std(write)
+
 fig = plt.figure()
 axes = Axes3D(fig)
 axes.scatter(math, read, write, color='#ef1234')
@@ -23,8 +42,13 @@ plt.show()
 
 X = np.c_[np.ones(m), math, read]
 beta = np.random.rand(3, 1)
+print("INITIAL BETA = " + str(beta))
 Y = write
 
-alpha = 0.01
+alpha, iterations = 0.01, 4000
 initial_cost = cost_function(X, Y, beta)
 print("INITIAL COST = " + str(initial_cost))
+
+beta, cost = gradient_descent(X, Y, beta, alpha, iterations)
+print("LEAST COST = " + str(cost))
+print("BETA at least cost = " + str(beta))
