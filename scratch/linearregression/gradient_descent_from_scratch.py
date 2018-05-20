21921df841f2beb587bb7ed0e01ef2cb651169b4
Gowtham Behara <gothsiddu@gmail.com>
2018-05-20 19:19:17 +0530

    Preprocess input data & write cost function

--- scratch/linearregression/gradient_descent_from_scratch.py
+++ scratch/linearregression/gradient_descent_from_scratch.py
@@ -3,14 +3,24 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
+
+def cost_function(x, y, beta):
+    return np.dot((np.dot(x, beta) - y).T, np.dot(x, beta) - y) / y.shape[0]
+
+
 data = pd.read_csv('../../data/student.csv')
 print(data.head())
 
-math = data['Math'].values
-read = data['Reading'].values
-write = data['Writing'].values
+m = data.shape[0]
+math = data['Math'].values.reshape((m, 1))
+read = data['Reading'].values.reshape((m, 1))
+write = data['Writing'].values.reshape((m, 1))
 
 fig = plt.figure()
 axes = Axes3D(fig)
 axes.scatter(math, read, write, color='#ef1234')
 plt.show()
+
+X = np.c_[np.ones(m), math, read]
+beta = np.random.rand(3, 1)
+Y = write
