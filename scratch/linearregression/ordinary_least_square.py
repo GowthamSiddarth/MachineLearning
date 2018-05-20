dc3a1079e0ac202a2193770d53d84ae685addacb
Gowtham Behara <gothsiddu@gmail.com>
2018-05-20 16:26:05 +0530

    Write covariance and variance functions

--- scratch/linearregression/ordinary_least_square.py
+++ scratch/linearregression/ordinary_least_square.py
@@ -7,3 +7,28 @@ plt.rcParams['figure.figsize'] = (20.0, 10.0)
 data = pd.read_csv('../../data/headbrain.csv')
 print(data.shape)
 print(data.head())
+
+X = data.ix[:, 2]
+y = data.ix[:, 3]
+
+print(X.shape)
+print(y.shape)
+
+
+def covariance(x, y):
+    if 1 == x.ndim:
+        x = x.reshape(x.shape[0], 1)
+    if 1 == y.ndim:
+        y = y.reshape(y.shape[0], 1)
+
+    m = y.shape[0]
+    return x * y - (np.sum(x) * np.sum(y)) / m
+
+
+def variance(x):
+    if 1 == x.ndim:
+        x = x.reshape(x.shape[0], 1)
+
+    m = x.shape[0]
+    return x.T * x - (np.sum(x) ** 2) / m
+
