03b406ab82753e0850f8928dc56c04b04346f905
Gowtham Behara <gothsiddu@gmail.com>
2018-05-20 19:01:23 +0530

    Visulaize the features from students.csv

--- scratch/linearregression/gradient_descent_from_scratch.py
+++ scratch/linearregression/gradient_descent_from_scratch.py
@@ -3,7 +3,14 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-plt.rcParams['figure.figsize'] = (20.0, 10.0)
-
 data = pd.read_csv('../../data/student.csv')
 print(data.head())
+
+math = data['Math'].values
+read = data['Reading'].values
+write = data['Writing'].values
+
+fig = plt.figure()
+axes = Axes3D(fig)
+axes.scatter(math, read, write, color='#ef1234')
+plt.show()
