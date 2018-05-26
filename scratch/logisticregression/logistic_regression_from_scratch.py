6e6658a2c7656cf98e710fd1b25a8552833b1a61
Gowtham Behara <gothsiddu@gmail.com>
2018-05-26 17:10:14 +0530

    Optimize weights & biases using gradient descent

--- scratch/logisticregression/logistic_regression_from_scratch.py
+++ scratch/logisticregression/logistic_regression_from_scratch.py
@@ -16,16 +16,16 @@ def get_preprocessed_data(data_set_type):
     data_set['Embarked'] = data_set['Embarked'].fillna('Q')
 
     data_set = encode_labels(data_set, ['Sex', 'Ticket', 'Cabin', 'Embarked'])
-    features = data_set[['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked']]
-    normalize_features(features)
+    features = ['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked']
+    features_normalized = normalize_features(data_set, features)
 
     if data_set_type == 'train':
         y_train = data_set['Survived']
-        y_train = y_train.reshape(y_train.shape[0], 1)
-        return features.as_matrix(), y_train
+        y_train = y_train.values.reshape(y_train.shape[0], 1)
+        return features_normalized.as_matrix(), y_train
     else:
         passenger_id = data_set['PassengerId']
-        return features.as_matrix(), passenger_id
+        return features_normalized.as_matrix(), passenger_id
 
     return x
 
@@ -39,18 +39,35 @@ def encode_labels(data_frame, labels):
     return data_frame
 
 
-def normalize_features(x_train):
-    for feature in x_train.columns:
-        x_train[feature] = (x_train[feature] - np.mean(x_train[feature])) / np.std(x_train[feature])
+def normalize_features(data_set, features):
+    for feature in features:
+        data_set.loc[:, feature] = (data_set[feature] - np.mean(data_set[feature])) / np.std(data_set[feature])
 
-    return x_train
+    return data_set[features]
+
+
+def sigmoid(x): return 1 / (1 + np.exp(-x))
 
 
 train_X, train_y = get_preprocessed_data('train')
 test_X, passenger_id = get_preprocessed_data('test')
 
-alpha, weights, bias, m = 0.01, np.random.normal(0, 0.1, 9), np.random.normal(0, 0.1, 9), train_y.shape[0]
+m, epochs = train_y.shape[0], 10000
+alpha, weights, bias = 0.01, np.random.normal(0, 0.1, 9).reshape(9, 1), np.random.normal(0, 0.1, m).reshape(m, 1)
 print(train_X.shape)
 print(train_y.shape)
 
+for epoch in range(1, epochs + 1):
+    Z = np.dot(train_X, weights) + bias
+    A = sigmoid(Z)
 
+    loss = np.sum(-(np.dot(train_y.T, np.log(A)) + np.dot(1 - train_y.T, np.log(1 - A)))) / m
+    if 0 == epoch % 100:
+        print("Cost at iteration " + str(epoch) + " is: " + str(loss))
+
+    dZ = A - train_y
+    dw = np.dot(train_X.T, dZ) / m
+    db = np.sum(dZ) / m
+
+    weights = weights - alpha * dw
+    bias = bias - alpha * db
