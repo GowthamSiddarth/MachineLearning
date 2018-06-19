af0ac63dc0045c080e0c3ce3d3736a671dd465a0
Gowtham Behara <gothsiddu@gmail.com>
2018-06-19 08:40:05 +0530

    Write method to get normalized train data

--- scratch/neuralnetwork/neural_network_from_scratch.py
+++ scratch/neuralnetwork/neural_network_from_scratch.py
@@ -1,5 +1,26 @@
 import numpy as np
 import pandas as pd
 
-train_data = pd.read_csv('../../data/hand-written-digit-recognition-train.csv')
-print(train_data.shape)
+
+def normalize(x): return (x - np.mean(x)) / np.std(x)
+
+
+# ../../data/hand-written-digit-recognition-train.csv
+def get_data(path, instances, normalization=True):
+    train_data = pd.read_csv(path)
+    train_X, train_Y = train_data.iloc[:instances, 1:], train_data.iloc[:instances, :1]
+
+    if normalization:
+        train_X = normalize(train_X)
+
+    return train_X, train_Y
+
+
+if __name__ == "__main__":
+    print("Starting...")
+    train_data_path = "../../data/hand-written-digit-recognition-train.csv"
+    instances = 10000
+
+    train_X, train_Y = get_data(train_data_path, instances)
+    print(train_X.shape)
+    print(train_Y.shape)
