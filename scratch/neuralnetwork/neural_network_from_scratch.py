56adcb9d27931b15aa49d928b8c8c27adb308592
Gowtham Behara <gothsiddu@gmail.com>
2018-06-20 10:15:45 +0530

    Random initialization of weights in network

--- scratch/neuralnetwork/neural_network_from_scratch.py
+++ scratch/neuralnetwork/neural_network_from_scratch.py
@@ -5,7 +5,6 @@ import pandas as pd
 def normalize(x): return (x - np.mean(x)) / np.std(x)
 
 
-# ../../data/hand-written-digit-recognition-train.csv
 def get_data(path, instances, normalization=True):
     train_data = pd.read_csv(path)
     train_X, train_Y = train_data.iloc[:instances, 1:], train_data.iloc[:instances, :1]
@@ -16,6 +15,20 @@ def get_data(path, instances, normalization=True):
     return train_X, train_Y
 
 
+def initialize_network(num_of_features, hidden_layers_nodes, num_of_outputs):
+    input_layer, output_layer = num_of_features, num_of_outputs
+    weights, prev_layer_nodes = {}, input_layer
+    for index in range(len(hidden_layers_nodes)):
+        current_layer_nodes = hidden_layers_nodes[index]
+        current_layer_weights = np.random.rand(current_layer_nodes, prev_layer_nodes)
+        weights[index + 1] = current_layer_weights
+        prev_layer_nodes = current_layer_nodes
+
+    output_layer_weights = np.random.rand(output_layer, prev_layer_nodes)
+    weights[len(hidden_layers_nodes)] = output_layer_weights
+    return weights
+
+
 if __name__ == "__main__":
     print("Starting...")
     train_data_path = "../../data/hand-written-digit-recognition-train.csv"
@@ -24,3 +37,7 @@ if __name__ == "__main__":
     train_X, train_Y = get_data(train_data_path, instances)
     print(train_X.shape)
     print(train_Y.shape)
+
+    weights = initialize_network(train_X.shape[1], [5, 3], 10)
+    for layer, weight_matrix in weights.items():
+        print("Layer " + str(layer) + ": " + str(weight_matrix.shape))
